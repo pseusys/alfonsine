5aac3ca8d6d82c9a529932b82ef666879bfc822c
milesartemius <shveitsar215@gmail.com>
2021-11-19 02:11:03 +0300

    website fixed

--- web/scripts/simple.ts
+++ web/scripts/simple.ts
@@ -11,6 +11,21 @@ import { saturn } from "../../code/saturn";
 import { dierum } from "../../code/utils";
 import { precession_model } from "../../code/sphere";
 
+const SIMPLE_SIGNS = {
+    "ARIES": "Ari",
+    "TAURUS": "Tau",
+    "GEMINI": "Gem",
+    "CANCER": "Cnc",
+    "LEO": "Leo",
+    "VIRGO": "Vir",
+    "LIBRA": "Lib",
+    "SCORPIUS": "Sco",
+    "SAGITTARIUS": "Sgr",
+    "CAPRICORNUS": "Cap",
+    "AQUARIUS": "Aqr",
+    "PISCES": "Psc"
+}
+
 
 const form = document.getElementById("inp") as HTMLFormElement
 const input = document.getElementById("calc") as HTMLButtonElement
@@ -29,7 +44,7 @@ function display (date_time: Date, diff_h: number, diff_m: number, east: boolean
         return
     }
 
-    const accuracy = acc ? 10 : 0
+    const accuracy = acc ? 0 : 10
     const difference: Difference = { hours: diff_h, minutes: diff_m, east: east }
     const days = dierum(date_time, difference, Epoch.find(epoch))
     const precession = precession_model(Precession[prec], days, accuracy)
@@ -48,7 +63,7 @@ function display (date_time: Date, diff_h: number, diff_m: number, east: boolean
         document.getElementById(`astronomic_minutes_${body}`).textContent = bodies[body].astronomic.minutes
         document.getElementById(`astrologic_degrees_${body}`).textContent = bodies[body].astrologic.degrees
         document.getElementById(`astrologic_minutes_${body}`).textContent = bodies[body].astrologic.minutes
-        document.getElementById(`sign_${body}`).textContent = bodies[body].sign
+        document.getElementById(`sign_${body}`).textContent = SIMPLE_SIGNS[bodies[body].sign]
         document.getElementById(`latitude_degrees_${body}`).textContent = bodies[body].latitude.degrees
         document.getElementById(`latitude_minutes_${body}`).textContent = bodies[body].latitude.minutes
         document.getElementById(`n_s_${body}`).textContent = bodies[body].north != null ? (bodies[body].north ? 'N' : 'S') : ''
